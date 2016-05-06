36b42a0f86014aec36762b83b845f962e2089ee9
Erin Bergman <Erin Bergman>
2016-05-06 09:42:21 -0400

    Basic launch template

---
+++ src/com/handleit/DevObj.java
@@ -0,0 +1,5 @@
+package com.handleit;
+
+public class DevObj {
+
+}
---
+++ src/com/handleit/LaunchIndex.java
@@ -0,0 +1,11 @@
+package com.handleit;
+
+public class LaunchIndex {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello HandleIT");
+		
+	}
+
+}
