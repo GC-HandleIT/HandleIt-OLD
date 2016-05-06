ed08fbe972c9ed7f39dac77b79c522cae14b973e
Erin Bergman <Erin Bergman>
2016-05-06 13:35:28 -0400

    Worked on DB Connection and added Web cont

--- src/com/handleit/DevObj.java
+++ src/com/handleit/DevObj.java
@@ -1,5 +1,7 @@
 package com.handleit;
 
 public class DevObj {
+	
+	
 
 }
--- src/com/handleit/ServletDevConn.java
+++ src/com/handleit/ServletDevConn.java
@@ -1,9 +1,65 @@
 package com.handleit;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ServletDevConn {
 
 	static Scanner sc = new Scanner(System.in);
-	public static ArrayList<>
+	public static ArrayList<DevObj> DevObjForConn = new ArrayList<>();
+
+	static final String JBDC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost:3306/?user=root?autoReconnect=true&useSSL=false";
+	static final String USER = "root";
+	static final String PASSWORD = "sesame";
+
+	private static Connection conn = null;
+	private static Statement stmt = null;
+	private static PreparedStatement prepStmt = null;
+	private static ResultSet resSet = null;
+
+	public static void initConnToDatabase() {
+
+		try {
+			Class.forName(JBDC_DRIVER);
+
+			System.out.println("Connecting to the Database");
+			conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			System.out.println(ex.getMessage());
+		}
+	}
+
+	public static DevObj searchForDev(String devLoginEmail) { //use this to log into the dev portal
+		
+		try {
+			initConnToDatabase();
+			stmt = conn.createStatement();
+			resSet = stmt.executeQuery("SELECT * FROM `db`.`dev table` ");
+			
+			while (resSet.next());
+				String devSearchParam = resSet.getString("`email`");
+				
+				if (devSearchParam.equalsIgnoreCase(devLoginEmail)) {
+					
+					DevObj devForLogin = new DevObj();
+					
+					
+					
+				}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+		
+		return null;
+		
+	}
 }
